9077ba1607485a5d80ef80feba88c13b248df23f
RoggerK <at76324897@idat.edu.pe>
2022-11-23 20:57:36 -0500

    avance carrito 2

--- app/src/main/java/idat/edu/pe/dm2/grupo1/megafarmakotlin/CarritoAdapter.kt
+++ app/src/main/java/idat/edu/pe/dm2/grupo1/megafarmakotlin/CarritoAdapter.kt
@@ -3,54 +3,65 @@ package idat.edu.pe.dm2.grupo1.megafarmakotlin
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import idat.edu.pe.dm2.grupo1.megafarmakotlin.pojo.MedicamentoResponse
 
-class CarritoAdapter : RecyclerView.Adapter<CarritoAdapter.ViewHolder>() {
+class CarritoAdapter(var listaAgregados: ArrayList<MedicamentoResponse>, var edtCantidad: EditText) :
+    RecyclerView.Adapter<CarritoAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         var itemImage: ImageView
         var itemTitle: TextView
-        var itemDetail: TextView
+        var itemPrec: TextView
+        var itemDelete: ImageView
 
         init {
             itemImage = itemView.findViewById(R.id.imgProducto1)
             itemTitle = itemView.findViewById(R.id.tvNombreProducto1)
-            itemDetail = itemView.findViewById(R.id.tvPrecio)
-
+            itemPrec = itemView.findViewById(R.id.tvPrecio)
+            itemDelete = itemView.findViewById(R.id.imvEliminar)
         }
     }
 
-    val titles = arrayOf(
-        "Vick 44",
-        "Chao Tableta ",
-        "Panadol "
-    )
-
-    val details = arrayOf(
-        "S/ 16.70",
-        "S/ 92.50",
-        "S/ 14.40"
-    )
-
-    val images = intArrayOf(
-        R.drawable.jarabe, R.drawable.chao, R.drawable.panadol, R.drawable.mejoral, R.drawable.vita
-    )
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val v = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.item_carrito, viewGroup, false)
+        edtCantidad.setText(itemCount.toString())
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-        viewHolder.itemTitle.text = titles[i]
-        viewHolder.itemDetail.text = details[i]
-        viewHolder.itemImage.setImageResource(images[i])
+        val id = listaAgregados[i].idproducto
+        val url = listaAgregados[i].imagen_producto
+        val nombre = listaAgregados[i].nombre_producto
+        val precio = listaAgregados[i].precio_unitario.toString()
+
+        Picasso.get()
+            .load(url)
+            .error(R.drawable.ic_launcher_foreground)
+            .into(viewHolder.itemImage)
+
+        viewHolder.itemTitle.text = nombre
+        viewHolder.itemPrec.text = precio
+
+        viewHolder.itemDelete.setOnClickListener(View.OnClickListener {
+            eliminarProducto(i)
+        })
     }
 
     override fun getItemCount(): Int {
-        return titles.size
+        return listaAgregados.size
+    }
+
+    private fun eliminarProducto(i: Int) {
+        listaAgregados.removeAt(i)
+        notifyItemRangeRemoved(i, itemCount)
+        edtCantidad.setText(itemCount.toString())
     }
 }
--- app/src/main/java/idat/edu/pe/dm2/grupo1/megafarmakotlin/CarritoFragment.kt
+++ app/src/main/java/idat/edu/pe/dm2/grupo1/megafarmakotlin/CarritoFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentResultListener
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,37 +18,36 @@ import idat.edu.pe.dm2.grupo1.megafarmakotlin.pojo.MedicamentoResponse
 class CarritoFragment : Fragment() {
 
     private val listaAgregados = ArrayList<MedicamentoResponse>()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var carritoAdapter: CarritoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         parentFragmentManager.setFragmentResultListener("llavePrincipal",
             this, FragmentResultListener { requestKey, bundle ->
-                try {
-                    val agregados = bundle.getStringArrayList("listaAgregado") as ArrayList<String>
-
-                    if (agregados.size != 0) {
-                        for (medicamento in agregados) {
-                            val array = medicamento.split(";")
-                            val id = array[0].toInt()
-                            val url = array[1]
-                            val nombre = array[2]
-                            val presentacion = array[3]
-                            val precio = array[4].toDouble()
-                            listaAgregados.add(
-                                MedicamentoResponse(
-                                    idproducto = id,
-                                    imagen_producto = url,
-                                    nombre_producto = nombre,
-                                    presentacion = presentacion,
-                                    precio_unitario = precio
-                                )
+                val agregados = bundle.getStringArrayList("listaAgregado") as ArrayList<String>
+                if (agregados.size == 0) {
+                    AppMessage.enviarMensaje(requireView(), "No hay productos agregados",
+                        TypeMessage.INFO)
+                } else {
+                    for (medicamento in agregados) {
+                        val array = medicamento.split(";")
+                        val id = array[0].toInt()
+                        val url = array[1]
+                        val nombre = array[2]
+                        val presentacion = array[3]
+                        val precio = array[4].toDouble()
+                        listaAgregados.add(
+                            MedicamentoResponse(
+                                idproducto = id,
+                                imagen_producto = url,
+                                nombre_producto = nombre,
+                                presentacion = presentacion,
+                                precio_unitario = precio
                             )
-                            //println("$id;$url;$nombre;$presentacion;$precio")
-                        }
+                        )
+                        println("$id;$url;$nombre;$presentacion;$precio")
                     }
-
-                } catch (n: NullPointerException) {
-                    AppMessage.enviarMensaje(requireView(), "No hay productos agregados",TypeMessage.INFO)
                 }
             })
     }
@@ -57,13 +57,13 @@ class CarritoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment_carrito, container, false)
-        var recyclerView: RecyclerView = view.findViewById(R.id.reCarrito)
+        var edtCantidad = view.findViewById<EditText>(R.id.edtCantidad)
+        recyclerView = view.findViewById(R.id.reCarrito)
         recyclerView.layoutManager = LinearLayoutManager(MyApplication.instance)
-        recyclerView.adapter = CarritoAdapter()
+        carritoAdapter = CarritoAdapter(listaAgregados, edtCantidad)
+        recyclerView.adapter = carritoAdapter
 
-        for (medicamento in listaAgregados) {
-            println("id: ${medicamento.idproducto} - nombre: ${medicamento.nombre_producto} - precio: ${medicamento.precio_unitario}")
-        }
+        edtCantidad.setText(carritoAdapter.itemCount.toString())
 
         return view
     }
